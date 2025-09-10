314538bc883659f5575bea7ac53c26efa59dff3a
HerbertNtim <herbertntim15@gmail.com>
2025-09-10 18:11:41 +0000

    first commit

---
+++ javascript/main.js
@@ -0,0 +1,8 @@
+const canvas = document.getElementById("myCanvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const A = { x: 200, y: 150 };
+const B = { x: 150, y: 250 };
+const C = { x: 50, y: 150 };
+const D = { x: 250, y: 200 };
